660823118b505a08337e38db9f41131067cd0999
Charles Liptaak <cliptaak@hotmail.com>
2018-09-12 14:23:36 -0700

    Add project files.

---
+++ ServiceBus1/WorkerRoleWithSBQueue2/WorkerRole-Writer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace Writer
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        // The name of your queue
+        const string QueueName = "ProcessingQueue";
+
+        // QueueClient is thread-safe. Recommended that you cache 
+        // rather than recreating it on every request
+        QueueClient Client;
+        ManualResetEvent CompletedEvent = new ManualResetEvent(false);
+
+        public override void Run()
+        {
+            Trace.WriteLine("Starting processing of messages");
+
+            for (int i = 0; i < 15; i++)
+
+            {
+                
+                DateTime CurrentTime = DateTime.Now;
+                BrokeredMessage msg = new BrokeredMessage("Message " + i.ToString() + "  " + CurrentTime.Hour + ":" + CurrentTime.Minute + ":" + CurrentTime.Second);
+                Client.Send(msg);
+                Trace.WriteLine("sent message number " + i.ToString());
+            };
+        }
+
+        public override bool OnStart()
+        {
+            // Set the maximum number of concurrent connections 
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            // Create the queue if it does not exist already
+            string connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+            if (!namespaceManager.QueueExists(QueueName))
+            {
+                namespaceManager.CreateQueue(QueueName);
+            }
+
+            // Initialize the connection to Service Bus Queue
+            Client = QueueClient.CreateFromConnectionString(connectionString, QueueName);
+            return base.OnStart();
+        }
+
+        public override void OnStop()
+        {
+            // Close the connection to Service Bus Queue
+            Client.Close();
+            CompletedEvent.Set();
+            base.OnStop();
+        }
+    }
+}
